b03a9a6ed310895b36587a49dc498b99bcef443c
Ehlers <mxii@users.noreply.github.com>
2016-09-09 11:07:58 +0200

    v0.0.2: added typings, made varibles and functions private

--- src/offclick.directive.ts
+++ src/offclick.directive.ts
@@ -73,25 +73,25 @@ export class OffClickDirective implements OnInit {
    private _id: number = OffClickDirective.ID++;
    private _component: any;
 
-   @Input('offClick') offClickHandler: () => void;
+   @Input('offClick') private _offClickHandler: () => void;
 
    constructor(private _view: ViewContainerRef, private _offClickService: OffClickService) { }
 
    ngOnInit() {
 
-      if (typeof (this.offClickHandler) !== 'function') return;
+      if (typeof (this._offClickHandler) !== 'function') return;
 
       // see here : https://github.com/angular/angular/issues/8277#issuecomment-216206046
 
       this._component = (<any>this._view)._element.parentView.context;
       if (this._component) {
-         this.offClickHandler = this.offClickHandler.bind(this._component);
+         this._offClickHandler = this._offClickHandler.bind(this._component);
       }
 
       this._offClickService.add(this._id, this._component);
 
       this._offClickService.DocumentClick.subscribe(
-         click => { this.offClickHandler(); }
+         click => { this._offClickHandler(); }
       );
    }
 
@@ -99,12 +99,12 @@ export class OffClickDirective implements OnInit {
       this._offClickService.remove(this._id);
    }
 
-   clickPrevent() {
+   private clickPrevent() {
       var otherComponents = this._offClickService.getAll().filter(oc => oc.id != this._id);
       otherComponents.forEach(oc => oc.component.clickedOutside());
    }
 
-   onClick($event: any) {
+   private onClick($event: any) {
       setTimeout(() => this.clickPrevent(), 100); // need a delay! min. 100! otherwise that component closes and our x/y click-pos isnt present any more!
 
       // clicked inside of our directive.. stop click handling!
